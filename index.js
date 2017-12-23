e426b11fec68df68ffc6ba1fe2f796905f7d8f9a
Arnaud Casamé <djcase001@gmail.com>
2017-12-23 03:50:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ function add(a, b){
   return a + b;
 }
 
+function substract(a, b){
+  return a - b;
+}
+
+function multiply(a, b){
+  return a * b;
+}
+
+function divide(a, b){
+  return a / b;
+}
+
 function makeInt(n){
   return parseInt(n, 10);
 }
